b9ec16e5004f4da4876bef2eb57f8662add96c3d
Neo Soir <leonard@neosoir.com>
2022-07-04 03:12:42 +0200

    [start] plugin to woocommerce

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pizza Tab
+
+
+
---
+++ pizza-tab.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *
+ * @link                https://neoslab.online
+ * @since               1.0.0
+ * @package             pizza-tab
+ *
+ * @wordpress-plugin
+ * Plugin Name:         Pizza Tab 
+ * Plugin URI:          https://neoslab.online
+ * Description:         Create a portafolio type gallery and also takes values of publications to display in frontend using shorcodes.
+ * Version:             1.0.0
+ * Requires at least:   6.0
+ * Requires PHP:        7.4
+ * Author:              Neos Lab
+ * Author URI:          https://neoslab.online
+ * License:             GPL2
+ * License URI:         https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:         bcpg-textdomain
+ * Domain Path:         /languages
+ */
+
+
+
+
+
+
+
+
+
+
+
+
