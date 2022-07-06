4d2693b0c11b724ab826eab627befbd0f9e257ba
Neo Soir <leonard@neosoir.com>
2022-07-06 18:13:50 +0200

    [tweak] css to components in product page

--- templates/admin/pizza-product.php
+++ templates/admin/pizza-product.php
@@ -58,8 +58,55 @@ $wc_products = wc_get_products([
                             </optgroup>
                         <?php endforeach; ?>
                     </select>
-
                 </div>
+            </div>
+            <div class="group-components" id="pizza_consists_block">
+                <?php if ($pizza_product_data && !empty($pizza_product_data['pizza']['base'])) : ?>
+                    <?php foreach ($pizza_product_data['pizza']['base'] as $component) : ?>
+                        <div class="group-component" data-id="<?php echo esc_attr($component['id']); ?>">
+                            <div class="component-header">
+                                <div class="component-details">
+                                    <span><?php echo esc_html($component['name']); ?></span>
+                                    <span><?php echo wc_price($component['price']); ?></span>
+
+
+                                </div>
+                                <div class="component-actions">
+                                    <span class="dashicons dashicons-edit edit-component"></span>
+
+                                </div>
+                            </div>
+                            <div class="component-body">
+                                <div class="component-img">
+                                    <img src="<?php echo esc_attr(wp_get_attachment_image_url($component['imageId'], 'medium')); ?>" alt="">
+
+                                </div>
+                            </div>
+                            <div class="component-body-collapse">
+
+                                <div class="form-group-full">
+                                    <label for=""><?php esc_html_e('Price', 'u-pizza'); ?></label>
+                                    <input type="text" name="pizza_base[<?php echo esc_attr($component['id']); ?>][price]" value="<?php echo esc_attr($component['price']); ?>">
+                                </div>
+                                <div class="form-group-full">
+                                    <label for=""><?php esc_html_e('Weight', 'u-pizza'); ?></label>
+                                    <input type="text" name="pizza_base[<?php echo esc_attr($component['id']); ?>][weight]" value="<?php echo esc_attr($component['weight']); ?>">
+                                </div>
+
+                                <div class="form-group-full">
+
+                                    <input type="checkbox" name="pizza_base[<?php echo esc_attr($component['id']); ?>][required]" <?php checked($component['required'], 1); ?>>
+                                    <label for=""><?php esc_html_e('Required', 'u-pizza'); ?></label>
+                                </div>
+                                <div class="form-group-full">
+
+                                    <input type="checkbox" name="pizza_base[<?php echo esc_attr($component['id']); ?>][visible]" <?php checked($component['visible'], 1); ?>>
+                                    <label for=""><?php esc_html_e('Visible', 'u-pizza'); ?></label>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                <?php endif; ?>
             </div>
         </div>
     </div> 
